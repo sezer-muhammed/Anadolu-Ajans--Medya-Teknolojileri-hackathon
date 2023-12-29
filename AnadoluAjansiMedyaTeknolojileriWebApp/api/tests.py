e332d58112a20a0d5e8eee3d2cf47acc4b290c48
sezer-muhammed <e230531@metu.edu.tr>
2023-12-29 09:56:27 +0300

    Add image, text, and voice upload test cases

--- AnadoluAjansiMedyaTeknolojileriWebApp/api/tests.py
+++ AnadoluAjansiMedyaTeknolojileriWebApp/api/tests.py
@@ -1,3 +1,58 @@
-from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
+from rest_framework.test import APITestCase
+from rest_framework import status
+from .models import ImageUpload, TextUpload, VoiceUpload
 
-# Create your tests here.
+class ImageUploadTestCase(APITestCase):
+    def setUp(self):
+        # Set up any initial data or settings you need for the tests
+        self.image = SimpleUploadedFile(name='test_image.jpg', content=b'', content_type='image/jpeg')
+        self.url = '/images/'
+
+    def test_create_image(self):
+        # Test creating an image through the API
+        response = self.client.post(self.url, {'image': self.image})
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(ImageUpload.objects.count(), 1)
+
+    def test_retrieve_images(self):
+        # Test retrieving images through the API
+        self.client.post(self.url, {'image': self.image})
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
+
+class TextUploadTestCase(APITestCase):
+    def setUp(self):
+        self.text = 'Sample text'
+        self.url = '/texts/'
+
+    def test_create_text(self):
+        response = self.client.post(self.url, {'text': self.text})
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(TextUpload.objects.count(), 1)
+
+    def test_retrieve_texts(self):
+        self.client.post(self.url, {'text': self.text})
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
+
+class VoiceUploadTestCase(APITestCase):
+    def setUp(self):
+        self.voice_file = SimpleUploadedFile(name='test_voice.mp3', content=b'Some audio content', content_type='audio/mpeg')
+        self.url = '/voices/'
+
+    def test_create_voice(self):
+        response = self.client.post(self.url, {'voice_file': self.voice_file})
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(VoiceUpload.objects.count(), 1)
+
+    def test_retrieve_voices(self):
+        self.client.post(self.url, {'voice_file': self.voice_file})
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
