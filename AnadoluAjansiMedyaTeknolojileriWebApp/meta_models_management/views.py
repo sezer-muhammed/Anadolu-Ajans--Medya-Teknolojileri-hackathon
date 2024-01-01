96639073565a9c0cd871cc2225ed552f27120724
sezer-muhammed <e230531@metu.edu.tr>
2024-01-01 21:26:17 +0300

    Fix redirect URL in HomeView

--- AnadoluAjansiMedyaTeknolojileriWebApp/meta_models_management/views.py
+++ AnadoluAjansiMedyaTeknolojileriWebApp/meta_models_management/views.py
@@ -16,7 +16,7 @@ class HomeView(View):
             # Save the form and handle the file and text uploads
             form.save()
             # Redirect to a new URL or the same with a success message (optional)
-            return redirect('/meta_models_management/home.html')  # Replace with the actual URL name for success
+            return redirect('/meta_models_management/home/')  # Replace with the actual URL name for success
 
         # If the form is not valid, render the page again with the form (errors will show)
         return render(request, 'meta_models_management/home.html', {'form': form})
