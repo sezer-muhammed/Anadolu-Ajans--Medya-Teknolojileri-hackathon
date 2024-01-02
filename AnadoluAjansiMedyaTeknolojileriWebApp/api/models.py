f7472a1949b22600ab09ce72cdab842255537040
sezer-muhammed <e230531@metu.edu.tr>
2024-01-02 12:38:42 +0300

    Add save method to models.py with callback logic

--- AnadoluAjansiMedyaTeknolojileriWebApp/api/models.py
+++ AnadoluAjansiMedyaTeknolojileriWebApp/api/models.py
@@ -1,11 +1,27 @@
 from django.db import models
 from .validators import validate_file_extension
 
+def my_callback(instance):
+    # Your callback logic here
+    pass
+
 class ImageUpload(models.Model):
     image = models.ImageField(upload_to='images/')
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        my_callback(self)
 
 class TextUpload(models.Model):
     text = models.TextField()
 
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        my_callback(self)
+
 class VoiceUpload(models.Model):
     voice_file = models.FileField(upload_to='voices/', validators=[validate_file_extension])
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        my_callback(self)
