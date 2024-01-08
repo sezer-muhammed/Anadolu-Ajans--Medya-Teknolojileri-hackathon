9fee7b0825c7be00df9ab609fccd97e6872f23ad
Muhammed Sezer <74321576+sezer-muhammed@users.noreply.github.com>
2024-01-08 13:49:04 +0300

    Add sphinxcontrib_django to requirements.txt

--- AnadoluAjansiMedyaTeknolojileriWebApp/docs/_build/html/_static/documentation_options.js
+++ AnadoluAjansiMedyaTeknolojileriWebApp/docs/_build/html/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: 'test',
+    VERSION: 'Version 1.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
--- AnadoluAjansiMedyaTeknolojileriWebApp/docs/conf.py
+++ AnadoluAjansiMedyaTeknolojileriWebApp/docs/conf.py
@@ -1,10 +1,4 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 import os
 import sys
@@ -13,40 +7,58 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'AnadoluAjansiMedyaTeknolojileriWebApp.se
 import django
 django.setup()
 
-
-project = 'test'
-copyright = '2024, test'
-author = 'test'
-release = 'test'
+project = 'Anadolu Ajansı Medya Teknolojileri Web App'
+copyright = '2024, Muhammed Sezer'
+author = 'Muhammed Sezer, Şevval Belkıs Dikkaya, Ahmet Sezer, Metehan İçöz'
+release = 'Version 1.0'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 
 html_theme = 'sphinx_rtd_theme'
 
 extensions = [
     'sphinx.ext.autodoc',  # Include documentation from docstrings
     'sphinx.ext.viewcode',  # Add links to source code
-    # Add any other extensions you might need
+    'sphinxcontrib_django',  # Specific for Django
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx_rtd_theme',
 ]
-extensions.append('sphinx.ext.intersphinx')
-extensions.append('sphinx.ext.todo')
-todo_include_todos = True
 
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3', None),
-    'django': ('https://docs.djangoproject.com/en/stable/', 'https://docs.djangoproject.com/en/stable/_objects/'),
-}
-todo_include_todos = True
+# Theme options and customization
 html_theme_options = {
     'navigation_depth': 4,
+    'display_version': True,
+    'collapse_navigation': True,  # Collapse navigation for a cleaner look
+    'sticky_navigation': True,  # Ensures the navigation pane stays while scrolling
 }
-html_theme_options['display_version'] = True
-
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-# conf.py
+
+# Intersphinx mapping for Django 5.0
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+    'django': ('https://docs.djangoproject.com/en/5.0/', None),
+}
+
+# Enable todos
+todo_include_todos = True
+
+# Sphinxcontrib Django
+# It's good to specify the settings here for your Django project
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+# Ensure that the theme is set to Read The Docs theme
 import sphinx_rtd_theme
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+# -- Options for sphinxcontrib_django ---------------------------------------
+
+# Document all available fields in models (default: True)
+sphinxcontrib_django_settings = {
+    'doc_fields': True,
+}
+
+# If you have a custom user model and want to document it properly
+django_settings_module = os.environ['DJANGO_SETTINGS_MODULE']
