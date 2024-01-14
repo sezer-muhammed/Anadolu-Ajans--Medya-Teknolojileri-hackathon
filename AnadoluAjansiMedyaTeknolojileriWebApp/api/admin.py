240ed5599fa32ad30f145a1d3d775ba3c35c5d3b
sezer-muhammed <e230531@metu.edu.tr>
2024-01-14 16:39:36 +0300

    Update admin.py to display images in ImageUploadAdmin

--- AnadoluAjansiMedyaTeknolojileriWebApp/api/admin.py
+++ AnadoluAjansiMedyaTeknolojileriWebApp/api/admin.py
@@ -1,13 +1,19 @@
 from django.contrib import admin
 from .models import ImageUpload, TextUpload, VoiceUpload
+from django.utils.html import mark_safe
 
-
-# Register your models here.
 @admin.register(ImageUpload)
 class ImageUploadAdmin(admin.ModelAdmin):
-    list_display = ["id", "image"]
+    list_display = ["id", "display_image"]
     list_filter = ["id"]
     search_fields = ["id"]
+
+    def display_image(self, obj):
+        if obj.image:
+            return mark_safe(f'<img src="{obj.image.url}" height="150" />')
+        return "No image"
+    display_image.short_description = "Image"
+    display_image.allow_tags = True
 
 
 @admin.register(TextUpload)
