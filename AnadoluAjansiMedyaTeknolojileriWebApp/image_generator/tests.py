449b98cc14127ca89fb9cb2fea0e157afa5779c2
Muhammed Sezer <74321576+sezer-muhammed@users.noreply.github.com>
2024-01-28 22:23:47 +0300

    Update test case and model creation in image_generator

--- AnadoluAjansiMedyaTeknolojileriWebApp/image_generator/tests.py
+++ AnadoluAjansiMedyaTeknolojileriWebApp/image_generator/tests.py
@@ -14,29 +14,58 @@ class ImageGenerationTestCase(TestCase):
       self.subcategory = Subcategory.objects.create(name='test')  # Create a Subcategory instance with the name 'test'
       self.object_detail = ObjectDetail.objects.create(description='test')  # Create an ObjectDetail instance with the description 'test'
       self.character = Character.objects.create(type='test', action='test')  # Create a Character instance with the type 'test' and action 'test'
+      self.character = Character.objects.create(
+          type='test', action='test', appearance='test'
+      )
+
+      # Create additional Keyword and Subcategory instances for NewsContext
+      self.keyword2 = Keyword.objects.create(word='test2')
+      self.subcategory2 = Subcategory.objects.create(name='test2')
+
+      # Update NewsContext instance creation
       self.news_context = NewsContext.objects.create(
-        headline='test',  # Create a NewsContext instance with the headline 'test'
-        category='test',  # Set the category of the NewsContext instance to 'test'
-        emotionTone='test',  # Set the emotionTone of the NewsContext instance to 'test'
-        emotionalIntensity=3,  # Set the emotionalIntensity of the NewsContext instance to 3
-        geographicalContext='test',  # Set the geographicalContext of the NewsContext instance to 'test'
-        temporalContext='test',  # Set the temporalContext of the NewsContext instance to 'test'
-        sourceCredibility=3,  # Set the sourceCredibility of the NewsContext instance to 3
-        audienceAgeGroup='test'  # Set the audienceAgeGroup of the NewsContext instance to 'test'
+          headline='test', 
+          category='test', 
+          emotionTone='test',
+          emotionalIntensity=3,
+          geographicalContext='test', 
+          temporalContext='test',
+          sourceCredibility=3, 
+          audienceAgeGroup='test',
+          audienceInterests='test'
       )
+      self.news_context.keywords.add(self.keyword, self.keyword2)
+      self.news_context.subcategories.add(self.subcategory, self.subcategory2)
+
+      # Update VisualElements instance creation
       self.visual_elements = VisualElements.objects.create(
-        mainSubject='test',  # Create a VisualElements instance with the mainSubject 'test'
-        backgroundScene='test',  # Set the backgroundScene of the VisualElements instance to 'test'
-        colorPalette='test',  # Set the colorPalette of the VisualElements instance to 'test'
-        texture='test'  # Set the texture of the VisualElements instance to 'test'
+          mainSubject='test', 
+          backgroundScene='test', 
+          colorPalette='test', 
+          texture='test',
+          dynamicElements='test', 
+          textOverlayStyle='test', 
+          motionEffects='test'
       )
+      self.visual_elements.characters.add(self.character)
+      self.visual_elements.object_details.add(self.object_detail)
+
+      # Update UserCustomizations instance creation
+      self.user_customizations = UserCustomizations.objects.create(
+          additionalText='test', 
+          userUploads='test', 
+          specificRequests='test', 
+          feedbackLoop='test', 
+          templates='test'
+      )     
+
       self.style_preferences = StylePreferences.objects.create(
-        artStyle='test',  # Create a StylePreferences instance with the artStyle 'test'
-        composition='test',  # Set the composition of the StylePreferences instance to 'test'
-        lighting='test',  # Set the lighting of the StylePreferences instance to 'test'
-        aspectRatio='test'  # Set the aspectRatio of the StylePreferences instance to 'test'
+          artStyle='test',
+          composition='test',
+          lighting='test',
+          aspectRatio='test'
       )
-      self.user_customizations = UserCustomizations.objects.create(additionalText='test')  # Create a UserCustomizations instance with the additionalText 'test'
+    
       self.image_generation = ImageGeneration.objects.create(
         news_context=self.news_context,  # Set the news_context of the ImageGeneration instance to the created NewsContext instance
         visual_elements=self.visual_elements,  # Set the visual_elements of the ImageGeneration instance to the created VisualElements instance
@@ -46,26 +75,26 @@ class ImageGenerationTestCase(TestCase):
 
 
     def test_setup_successful(self):
-            # Check if the user is created successfully
-            self.assertEqual(User.objects.count(), 1)
-            self.assertEqual(self.user.username, 'testuser')
-            self.assertEqual(self.user.check_password('12345'), True)
+        # Check if the user is created successfully
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(self.user.username, 'testuser')
+        self.assertEqual(self.user.check_password('12345'), True)
 
-            # Check if the models are created successfully
-            self.assertEqual(Keyword.objects.count(), 1)
+        # Check if the models are created successfully
+        self.assertEqual(Keyword.objects.count(), 2)  # Two keywords are created
+        self.assertEqual(Subcategory.objects.count(), 2)  # Two subcategories are created
+        self.assertEqual(ObjectDetail.objects.count(), 1)
+        self.assertEqual(Character.objects.count(), 2)
+        self.assertEqual(NewsContext.objects.count(), 1)
+        self.assertEqual(VisualElements.objects.count(), 1)
+        self.assertEqual(StylePreferences.objects.count(), 1)
+        self.assertEqual(UserCustomizations.objects.count(), 1)
+        self.assertEqual(ImageGeneration.objects.count(), 1)
 
-            self.assertEqual(Subcategory.objects.count(), 1)
+        # Check relationships in NewsContext
+        self.assertEqual(self.news_context.keywords.count(), 2)
+        self.assertEqual(self.news_context.subcategories.count(), 2)
 
-            self.assertEqual(ObjectDetail.objects.count(), 1)
-
-            self.assertEqual(Character.objects.count(), 1)
-
-            self.assertEqual(NewsContext.objects.count(), 1)
-
-            self.assertEqual(VisualElements.objects.count(), 1)
-
-            self.assertEqual(StylePreferences.objects.count(), 1)
-
-            self.assertEqual(UserCustomizations.objects.count(), 1)
-
-            self.assertEqual(ImageGeneration.objects.count(), 1)
+        # Check relationships in VisualElements
+        self.assertEqual(self.visual_elements.characters.count(), 1)
+        self.assertEqual(self.visual_elements.object_details.count(), 1)
