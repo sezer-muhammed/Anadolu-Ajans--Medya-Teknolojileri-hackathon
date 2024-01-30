e1a2ffe91a48a3aa5ea6515d78268eb29d047363
Muhammed Sezer <74321576+sezer-muhammed@users.noreply.github.com>
2024-01-30 15:26:49 +0300

    Add image generation functionality to GUI views and models

--- AnadoluAjansiMedyaTeknolojileriWebApp/GUI/urls.py
+++ AnadoluAjansiMedyaTeknolojileriWebApp/GUI/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import MediaUploadView
+from .views import *
 
 urlpatterns = [
     path('upload/', MediaUploadView.as_view(), name='upload_media'),
+    path('image-generations/', ImageGenerationListView.as_view(), name='image-generation-list'),
+    path('imagegeneration-detail/<int:pk>/', ImageGenerationDetailView.as_view(), name='imagegeneration_detail'),
+    path('generate-images/<int:pk>/', GenerateImagesView.as_view(), name='generate_images'),
+
 ]
--- AnadoluAjansiMedyaTeknolojileriWebApp/GUI/views.py
+++ AnadoluAjansiMedyaTeknolojileriWebApp/GUI/views.py
@@ -1,7 +1,51 @@
 from django.views import View
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import ImageUploadForm, TextUploadForm
 from api.models import TextUpload, ImageUpload
+from django.http import HttpResponse
+
+from django.views.generic.list import ListView
+from image_generator.models import ImageGeneration, GeneratedImages
+
+class GenerateImagesView(View):
+    def get(self, request, pk):
+        # Placeholder for actual image generation implementation
+        # You would trigger the image generation process here and then redirect or update the response
+        return HttpResponse(f"Generate images for ImageGeneration with id {pk}")
+
+class ImageGenerationDetailView(View):
+    def get(self, request, pk):
+        # Retrieve the ImageGeneration instance using pk
+        image_generation = get_object_or_404(ImageGeneration, pk=pk)
+
+        # Check if either image_upload or text_upload is not null or empty
+        if image_generation.image_upload:
+            upload_info = image_generation.image_upload.image.url
+        elif image_generation.text_upload:
+            upload_info = image_generation.text_upload.text
+        else:
+            upload_info = "No additional uploads provided."
+
+        # Retrieve all images associated with the ImageGeneration
+        generated_images = GeneratedImages.objects.filter(image_generation=image_generation)
+
+        # Render the template with the data
+        return render(request, 'GUI/image_generation_detail.html', {
+            'image_generation': image_generation,
+            'upload_info': upload_info,
+            'generated_images': generated_images,
+        })
+
+class ImageGenerationListView(ListView):
+    model = ImageGeneration
+    template_name = 'GUI/image_generation_list.html'  # Your template name
+    context_object_name = 'image_generations'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Add any additional context if necessary
+        return context
+
 
 class MediaUploadView(View):
     def get(self, request, *args, **kwargs):
